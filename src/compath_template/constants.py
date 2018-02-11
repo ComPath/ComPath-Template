61062c03fb08818e62aa57b9d76c16ec1acbce19
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2018-02-11 11:56:55 +0100

    Update constants.py

--- src/compath_template/constants.py
+++ src/compath_template/constants.py
@@ -6,6 +6,7 @@ import os
 
 from bio2bel.utils import get_connection, get_data_dir
 
+# TODO: Change the name of the package to the new repo (e.g., wikipathways, reactome)
 MODULE_NAME = 'compath_template'
 DATA_DIR = get_data_dir(MODULE_NAME)
 DEFAULT_CACHE_CONNECTION = get_connection(MODULE_NAME)
